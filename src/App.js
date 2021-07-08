a2cebbf3749156d31e06d03445fae5d7a08bc06c
rurkowsky <74063543+rurkowsky@users.noreply.github.com>
2021-07-08 14:43:12 +0200

    dd

--- src/App.js
+++ src/App.js
@@ -7,19 +7,20 @@ export default class Wrapper extends React.Component{
     }
   constructor(token) {
     super(token);
-    this.token = " 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"
+    this.token = "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"
     this.client = axios.create({
-      baseURL: 'https://api.github.com/',
+      baseURL: 'https://gist.github.com/rurkowsky/5f432739e63bf4bf06e97d69908b793a.js',
       responseType: 'json',
       headers: {
         'Accept': 'application/vnd.github.v3+json',
         'Authorization': 'token ' + this.token,
         'Access-Control-allow-origin' : '*',
+        'X-Custom-Header': this.token,
         
       }
     })
   }
-
+/*
   getRequest(path) {
     return this.client.get(path)
   }
@@ -38,28 +39,48 @@ export default class Wrapper extends React.Component{
 
   getGist(gistId) {
     return this.getRequest(`/gists/${gistId}`)
-  }
+  }*/
   componentDidMount() {
-     const URL = `https://gist.github.com/rurkowsky/5f432739e63bf4bf06e97d69908b793a`
-     axios.get(URL, {'headers':{'Authorization': "token"+ this.token} })
+     axios.get('https://api.github.com/users/rurkowsky/gists',{
+      responseType: 'json',
+      headers: {
+        /*'Accept': 'application/vnd.github.v3+json',
+        'Authorization': "token"+ this.token,
+        'Access-Control-allow-origin' : '*',
+        'X-Custom-Header': this.token,
+        'Content-Type': 'application/json'
+        */
+      },
+      
+      })
       .then(response => {
         const gists = response.data;
         this.setState({gists})
+        console.log(response.data);
+        console.log(response.config);
       })
       .catch((error) => {
         console.log(error);
       });
 };
-    
+  
 
   
     
 render(){
   return(
+    /*<div>
+     { this.state.gists.map(gist => <p>{gist.id}</p>)}
+     
+    </div>*/
+
     <div>
-     { this.state.gists.map(gist => <li>{gist}</li>)}
-  
+      {React.Children.map(children, function(gists)}
     </div>
    
+  
+  
+  
     );
-}};
+}
+};
